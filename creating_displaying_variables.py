6558c55bcbe6a087eb7e279580cc567e8a009939
Pitshou Asingalembi <Pitshou.Asingalembi@gmail.com>
2020-03-14 18:11:53 +0000

    pushing more files

---
+++ creating_displaying_variables.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3.7
+
+first_name = "Judd"
+last_name  = "Todd"
+age = 20
+birth_date = "02/10/2000"
+
+print(f"My name is {first_name} {last_name}. I was born on {birth_date}, and I'm {age} years old.")
